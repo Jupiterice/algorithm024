d94421ab73f6ddeb321d51e7a69866c95f4f2f8d
IceFour <ice_4_@126.com>
2021-01-31 23:18:42 +0800

    week01

---
+++ Week_01/20.有效的括号.js
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (43.64%)
+ * Likes:    2129
+ * Dislikes: 0
+ * Total Accepted:    517.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "()"
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "(]"
+ * 输出：false
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：s = "([)]"
+ * 输出：false
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：s = "{[]}"
+ * 输出：true
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * s 仅由括号 '()[]{}' 组成
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const parentMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+      }
+      const reduced = s.split('').reduce((pre, str) => {
+        const matchStr = parentMap[str];
+        if (matchStr) {
+          if (pre[pre.length - 1] === matchStr) {
+            pre.pop();
+          } else {
+            pre.push(str);
+          }
+        } else {
+          pre.push(str);
+        }
+        return pre;
+      }, []);
+      return reduced.length === 0;
+};
+// @lc code=end
+
